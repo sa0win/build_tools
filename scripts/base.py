02100829c2f29909a6081478c32763b1d6389985
sa0win <sascha.winandy@mailbox.org>
2024-07-18 23:01:41 +0200

    Update Server Repro

--- scripts/base.py
+++ scripts/base.py
@@ -498,10 +498,13 @@ def set_cwd(dir):
 # git ---------------------------------------------------
 def git_update(repo, is_no_errors=False, is_current_dir=False, git_owner=""):
   print("[git] update: " + repo)
-  owner = git_owner if git_owner else "ONLYOFFICE"
-  url = "https://github.com/" + owner + "/" + repo + ".git"
+  url = "https://github.com/ONLYOFFICE/" + repo + ".git"
+  if (repo == "server"):
+    url = "https://github.com/sa0win/" + repo + ".git"
   if config.option("git-protocol") == "ssh":
     url = "git@github.com:ONLYOFFICE/" + repo + ".git"
+    if (repo == "server"):
+      url = "git@github.com:sa0win/" + repo + ".git"
   folder = get_script_dir() + "/../../" + repo
   if is_current_dir:
     folder = repo
